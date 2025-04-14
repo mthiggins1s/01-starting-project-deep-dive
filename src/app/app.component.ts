f5cdd55d6c5fc4a5adbda199c707e70f6dd1fee3
Matthew Higgins <mthiggins1s@semo.edu>
2025-04-14 00:16:55 -0500

    feat: add new ticket component and integrate into tickets dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { ServerStatusComponent } from "./dashboard/server-status/server-status.c
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
+import { NewTicketComponent } from "./dashboard/tickets/new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, TicketsComponent, DashboardItemComponent, NewTicketComponent],
 })
 export class AppComponent {
   
